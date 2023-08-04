448a6dd3de2032f5cee96bf52561883444e061a0
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2023-08-03 21:33:59 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let sky;
+let moon;
+
+function preload() {
+  sky = loadImage("starmap_4k.jpg");
+  moon = loadImage("moonmap1k.jpg");
+}
+
+function setup() {
+  createCanvas(1366, 768, WEBGL);
+  background(0);
+}
+
+function draw() {
+   background(0);
+   translate(0, 0, 0);
+   rotateY(-frameCount/3000);
+   texture(sky);
+   sphere(3000);
+  push();
+   rotateY(frameCount/1000);
+   texture(moon);
+   noStroke();
+   sphere(300);
+  pop();
+}
