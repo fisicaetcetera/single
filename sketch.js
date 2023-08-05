77044d4f5efa73b6b84b8612244dee300dea42ce
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2023-08-05 16:05:38 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let sky;
 let moon;
+let rotation = 1;
 
 function preload() {
   sky = loadImage("starmap_4k.jpg");
@@ -9,16 +10,18 @@ function preload() {
 function setup() {
   createCanvas(1366, 768, WEBGL);
   background(0);
+  pare = createButton("Pare/Gire");
+  pare.mousePressed(Pare/Gire);
 }
 
 function draw() {
    background(0);
    translate(0, 0, 0);
-   rotateY(-frameCount/3000);
+   rotateY(-frameCount/3000 * rotation);
    texture(sky);
    sphere(3000);
   push();
-   rotateY(frameCount/1000);
+   rotateY(frameCount/1000 * rotation);
    texture(moon);
    noStroke();
    sphere(300);
@@ -32,4 +35,11 @@ function mouseClicked(){
     console.log("NOT looping");
     loop();
 }
+}
+fuction Pare(){
+  if(rotation == 1){
+  rotation = 0; 
+  } else {
+  rotation = 1;
+  }
 }
